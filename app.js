653493547fc0dce43150e9efbfec8d36077a02a7
JamesMcGuigan <james.mcguigan.github@gmail.com>
2021-04-06 10:24:16 +0100

    Quiz 1 - SHA256 hashing

--- app.js
+++ app.js
@@ -7,7 +7,9 @@
  * Step 2: Import from crypto-js module the sha265 library
  */
 
-// Write your code here
+
+// import sha256 from 'crypto-js/sha256';
+let SHA256 = require("crypto-js/sha256");
 
 /**
  * Variables: Do not change variable values to be able to answer the quiz
@@ -27,7 +29,7 @@ const dataObject = {
  */
 
 function generateHash(obj) {
-	// Write your code here
+	return SHA256(JSON.stringify(obj));
 }
 
 console.log(`SHA256 Hash: ${generateHash(data1)}`);
